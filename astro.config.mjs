1793504fb3782fbd88e38882dc305e54c84f73f1
NicolaM94 <nicola.moro2312@gmail.com>
2025-09-27 20:43:55 +0200

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://NicolaM94.github.io",
     markdown: {
         syntaxHighlight:"prism",
         remarkPlugins: [remarkMath],
